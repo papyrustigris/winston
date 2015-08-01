34c05ad636338efc80941b1f6ec643a414d8d1ab
matthew <matthew@matthews-MacBook-Pro.local>
2015-08-01 18:06:57 -0400

    basic user registration/login

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('app')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+		<h1>Welcome to FindYourWinston.com!</h1>
+		</div>
+	</div>
+</div>
+@endsection
