2d3cbac461f81b3ed8a08a0db70987d28622c1a6
matthew <matthew@matthews-MacBook-Pro.local>
2015-08-07 19:30:27 -0400

    add basic structure to site with multiple section landing page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,10 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel</title>
+	<title>Winston</title>
 
 	<link href="/css/app.css" rel="stylesheet">
+	<link href="/css/custom.css" rel="stylesheet">
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
@@ -20,7 +21,7 @@
 </head>
 <body>
 	<nav class="navbar navbar-default">
-		<div class="container-fluid">
+		<div class="container">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 					<span class="sr-only">Toggle Navigation</span>
@@ -28,7 +29,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Find Your Winston</a>
+				<a class="navbar-brand" href="#">Winston</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -38,8 +39,9 @@
 
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
-						<li><a href="/auth/login">Login</a></li>
-						<li><a href="/auth/register">Register</a></li>
+						<li id="#login-nav"><a href="/auth/login">Login/Register</a></li>
+						<li id="#about-nav"><a href="/auth/register">About</a></li>
+						<li id="#contact-nav"><a href="/auth/register">Contact</a></li>
 					@else
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,41 @@
 @extends('app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
 	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-		<h1>Welcome to FindYourWinston.com!</h1>
+		<div class="placeholder-bkgrnd">
+		</div>
+	</div>
+</div>
+
+<div id="about-section" class="container-fluid">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-8 col-xs-offset-2">
+				<pre>hello motherfucking world</pre>				
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="next-section" class="container-fluid">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-8 col-xs-offset-2">
+				<code>test test test test</code>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="footer-section">
+	<div class="container-fluid">
+		<div class="container">
+			<div class="row">
+				<footer>
+					<p>&copy;2015 Winston LLC</p>
+				</footer>
+			</div>
 		</div>
 	</div>
 </div>
